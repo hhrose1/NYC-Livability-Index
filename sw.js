422f9e94b46c3d5097a436c674ad63d57cd5ed64
Hannah Collins <hannahcollins@Hannahs-MacBook-Air-9.local>
2026-04-24 19:59:44 -0400

    Bump SW cache to v12 after revert

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * Also intercepts old neighborhood HTML URLs and redirects to neighborhood.html?slug=...
  */
 
-const CACHE_NAME = 'nyc-livability-v10';
+const CACHE_NAME = 'nyc-livability-v12';
 
 const PRECACHE_URLS = [
   '/',
